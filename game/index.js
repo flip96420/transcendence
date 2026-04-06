8ebd9a0aecefc0b2c81aa38399cc69fbbeb35d1f
Philipp Schmunk <philipp.schmunk@yahoo.de>
2026-04-06 23:23:11 +0200

    added index.js file

---
+++ game/index.js
@@ -0,0 +1,439 @@
+export function moveWorm()
+{
+    
+}
+
+export function spawnWorms(scene, num_players)
+{
+    let group1 = 
+    [
+        new BABYLON.Vector3(-36, -15, 0),
+        new BABYLON.Vector3(-34, -15, 0),
+        new BABYLON.Vector3(-32, -14, 0)
+    ];
+
+    let group2 = 
+    [
+        new BABYLON.Vector3(-35, 3, 0),
+        new BABYLON.Vector3(-33, 3, 0),
+        new BABYLON.Vector3(-31, 3, 0)
+    ];
+
+    let group3 = 
+    [
+        new BABYLON.Vector3(-33.5, 14, 0),
+        new BABYLON.Vector3(-30, 18, 0)
+    ];
+
+    let group4 =
+    [
+        new BABYLON.Vector3(-26.5, 22, 0),
+        new BABYLON.Vector3(-24.5, 21.5, 0),
+        new BABYLON.Vector3(-22.5, 21, 0)
+    ]
+
+    let group5 = 
+    [
+        new BABYLON.Vector3(-24, 9, 0),
+        new BABYLON.Vector3(-22, 8, 0),
+        new BABYLON.Vector3(-20, 8, 0),
+        new BABYLON.Vector3(-18, 8, 0),
+        new BABYLON.Vector3(-16, 8, 0)
+    ]
+
+    let group6 =
+    [
+        new BABYLON.Vector3(-27.5, -3, 0),
+        new BABYLON.Vector3(-25.8, -2.8, 0)
+    ]
+
+    let group7 = 
+    [
+        new BABYLON.Vector3(-24, 1, 0),
+        new BABYLON.Vector3(-22, 1, 0),
+        new BABYLON.Vector3(-20, 1, 0),
+        new BABYLON.Vector3(-18, 1, 0),
+    ]
+
+    let group8 =
+    [
+        new BABYLON.Vector3(-16, 0.5, 0),
+        new BABYLON.Vector3(-14, 0, 0),
+        new BABYLON.Vector3(-12, -0.5, 0)
+    ]
+
+    let group9 =
+    [
+        new BABYLON.Vector3(-21, -16, 0),
+        new BABYLON.Vector3(-18.5, -24, 0)
+    ]
+
+    let group10 = 
+    [
+        new BABYLON.Vector3(-16, -16, 0),
+        new BABYLON.Vector3(-14, -14.7, 0),
+        new BABYLON.Vector3(-12, -14.5, 0),
+        new BABYLON.Vector3(-10, -14.5, 0)
+    ]
+
+    let group11 =
+    [
+        new BABYLON.Vector3(-8, -14.5, 0),
+        new BABYLON.Vector3(-6, -14.5, 0),
+        new BABYLON.Vector3(-4, -15.5, 0),
+        new BABYLON.Vector3(-2, -16.5, 0),
+        new BABYLON.Vector3(-0, -17.5, 0)
+    ]
+
+    let group12 = 
+    [
+        new BABYLON.Vector3(3, -22.5, 0),
+        new BABYLON.Vector3(5, -22.7, 0),
+        new BABYLON.Vector3(7, -22, 0),
+    ]
+
+    let group13 =
+    [
+        new BABYLON.Vector3(9, -21, 0),
+        new BABYLON.Vector3(11, -22, 0),
+        new BABYLON.Vector3(13, -23, 0)
+    ]
+
+    let group14 =
+    [
+        new BABYLON.Vector3(-1, 7, 0),
+        new BABYLON.Vector3(1, 7.5, 0),
+        new BABYLON.Vector3(3, 8.2, 0),
+        new BABYLON.Vector3(5, 8.9, 0),
+        new BABYLON.Vector3(7, 9.6, 0)
+    ]
+
+    let group15 =
+    [
+        new BABYLON.Vector3(10.5, 4, 0),
+        new BABYLON.Vector3(12.5, 3, 0),
+        new BABYLON.Vector3(14.5, 2.2, 0),
+        new BABYLON.Vector3(16.5, 2.5, 0)
+    ]
+
+    let group16 = 
+    [
+        new BABYLON.Vector3(13, 24.2, 0),
+        new BABYLON.Vector3(15, 24.2, 0),
+        new BABYLON.Vector3(17, 24, 0),
+    ]
+
+    let group17 =
+    [
+        new BABYLON.Vector3(19, 24, 0),
+        new BABYLON.Vector3(21, 24, 0),
+        new BABYLON.Vector3(23, 24, 0)
+    ]
+
+    let group18 = 
+    [
+        new BABYLON.Vector3(20.5, 28, 0),
+        new BABYLON.Vector3(22.5, 28.7, 0),
+        new BABYLON.Vector3(24.5, 28.7, 0),
+        new BABYLON.Vector3(26.5, 28.5, 0),
+    ]
+
+    let group19 =
+    [
+        new BABYLON.Vector3(28.5, 28.5, 0),
+        new BABYLON.Vector3(30.5, 28.5, 0),
+        new BABYLON.Vector3(32.5, 28.3, 0)
+    ]
+
+    let group20 =
+    [
+        new BABYLON.Vector3(29.5, 12.7, 0),
+        new BABYLON.Vector3(31.5, 13, 0),
+        new BABYLON.Vector3(33.5, 13.3, 0),
+        new BABYLON.Vector3(35.5, 13.6, 0),
+        new BABYLON.Vector3(37.5, 13.9, 0)
+    ]
+
+    let group21 =
+    [
+        new BABYLON.Vector3(29, -1, 0),
+        new BABYLON.Vector3(31, -0.7, 0),
+        new BABYLON.Vector3(33, -0.4, 0),
+        new BABYLON.Vector3(35, -0.1, 0),
+        new BABYLON.Vector3(37, -1.3, 0)
+    ]
+
+    let group22 = 
+    [
+        new BABYLON.Vector3(32.3, -14.3, 0),
+        new BABYLON.Vector3(32.3, -14.3, 0),
+        new BABYLON.Vector3(34.3, -14.6, 0)
+    ]
+
+    let group23 =
+    [
+        new BABYLON.Vector3(28.5, -24.1, 0),
+        new BABYLON.Vector3(30.5, -24.6, 0)
+    ]
+
+    let groups =
+    [
+        group1,
+        group2,
+        group3,
+        group4,
+        group5,
+        group6,
+        group7,
+        group8,
+        group9,
+        group10,
+        group11,
+        group12,
+        group13,
+        group14,
+        group15,
+        group16,
+        group17,
+        group18,
+        group19,
+        group20,
+        group21,
+        group22,
+        group23
+    ]
+
+    let colors =
+    [
+        new BABYLON.Color4(1, 0.25, 0.25),
+        new BABYLON.Color4(0.29, 0.29, 0.9),
+        new BABYLON.Color4(0.38, 0.99, 0.38),
+        new BABYLON.Color4(0.98, 0.98, 0.16)
+    ]
+
+    for (let p = 0; p < num_players; p++)
+    {
+        let i = 0;
+        while (i < 5)
+        {
+            let num = Math.floor(BABYLON.Scalar.RandomRange(0, 10));
+            while (!groups[num])
+                num = Math.floor(BABYLON.Scalar.RandomRange(0, 10));
+            
+            let spawn_num = Math.floor(BABYLON.Scalar.RandomRange(1, 2));
+            for (let j = spawn_num; j > 0; j--)
+            {
+                let pos = Math.floor(BABYLON.Scalar.RandomRange(0, (groups[num].length)));
+                while (!groups[num][pos])
+                    pos = Math.floor(BABYLON.Scalar.RandomRange(0, (groups[num].length)));
+
+                var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 1, segments: 32}, scene);
+                sphere.position = groups[num][pos];
+
+                var material = new BABYLON.StandardMaterial("material", scene);
+                material.emissiveColor = colors[p];
+
+                sphere.material = material;
+
+                groups[num].splice(pos, 1);
+            }
+            groups.splice(num, 1);
+            i = i + spawn_num;
+        }
+    }
+}
+
+export class DestructibleGround
+{
+    constructor(scene, array)
+    {
+		this.scene = scene;
+		this.array = array;
+		this.depth = 1;
+		this.name = "ground";
+		if (this.array.length > 0) {	
+			this.groundMesh = BABYLON.MeshBuilder.ExtrudePolygon(this.name, {shape: this.array, depth: this.depth}, this.scene);
+			this.groundMesh.rotation.x = -Math.PI / 2;
+			this.groundMesh.position = new BABYLON.Vector3(-62, 0, -this.depth / 2);
+			this.groundMesh.physicsAggregate = new BABYLON.PhysicsAggregate(this.groundMesh, BABYLON.PhysicsShapeType.MESH, { mass: 0 }, this.scene);
+		}
+		else
+			this.groundMesh = null;
+	}
+}
+
+export function generateGround(scene)
+{
+	let array =
+	[
+        new BABYLON.Vector3(93.06, 0, -27.74),
+        new BABYLON.Vector3(93.02, 0, -25.4),
+        new BABYLON.Vector3(89.12, 0, -24.2),
+        new BABYLON.Vector3(89.22, 0, -21.54),
+        new BABYLON.Vector3(99.42, 0, -20.64),
+        new BABYLON.Vector3(97.88, 0, -15.8),
+        new BABYLON.Vector3(91.28, 0, -14.6),
+        new BABYLON.Vector3(91.04, 0, -12.06),
+        new BABYLON.Vector3(96.48, 0, -8.9),
+        new BABYLON.Vector3(99.54, 0, -6.26),
+        new BABYLON.Vector3(99.14, 0, -2.56),
+        new BABYLON.Vector3(97.28, 0, -1.2),
+        new BABYLON.Vector3(89.98, 0, -2.1),
+        new BABYLON.Vector3(88.68, 0, -0.2),
+        new BABYLON.Vector3(92.14, 0, 5.44),
+        new BABYLON.Vector3(95.04, 0, 8.24),
+        new BABYLON.Vector3(99.84, 0, 9.54),
+        new BABYLON.Vector3(100.44, 0, 13.04),
+        new BABYLON.Vector3(90.3, 0, 11.74),
+        new BABYLON.Vector3(90.1, 0, 14.24),
+        new BABYLON.Vector3(93.1, 0, 17.54),
+        new BABYLON.Vector3(97.2, 0, 18.24),
+        new BABYLON.Vector3(98.7, 0, 19.94),
+        new BABYLON.Vector3(97.9, 0, 24.24),
+        new BABYLON.Vector3(94.7, 0, 27.44),
+        new BABYLON.Vector3(84.0, 0, 27.84),
+        new BABYLON.Vector3(81.16, 0, 26.24),
+        new BABYLON.Vector3(85.26, 0, 25.54),
+        new BABYLON.Vector3(87.76, 0, 24.04),
+        new BABYLON.Vector3(84.16, 0, 22.84),
+        new BABYLON.Vector3(73.86, 0, 23.24),
+        new BABYLON.Vector3(74.16, 0, 19.74),
+        new BABYLON.Vector3(80.36, 0, 17.24),
+        new BABYLON.Vector3(80.46, 0, 5.8),
+        new BABYLON.Vector3(79.06, 0, 1.8),
+        new BABYLON.Vector3(75.52, 0, 1.24),
+        new BABYLON.Vector3(71.82, 0, 3.54),
+        new BABYLON.Vector3(70.32, 0, 9.14),
+        new BABYLON.Vector3(59.82, 0, 5.64),
+        new BABYLON.Vector3(60.22, 0, 2.64),
+        new BABYLON.Vector3(62.72, 0, 2.44),
+        new BABYLON.Vector3(63.92, 0, -0.36),
+        new BABYLON.Vector3(75.72, 0, -2.86),
+        new BABYLON.Vector3(77.82, 0, -7.22),
+        new BABYLON.Vector3(75.42, 0, -14.72),
+        new BABYLON.Vector3(78.22, 0, -20.92),
+        new BABYLON.Vector3(76.42, 0, -25.22),
+        new BABYLON.Vector3(71.02, 0, -21.72),
+        new BABYLON.Vector3(68.02, 0, -23.52),
+        new BABYLON.Vector3(63.92, 0, -22.92),
+        new BABYLON.Vector3(63.12, 0, -19.42),
+        new BABYLON.Vector3(58.92, 0, -16.82),
+        new BABYLON.Vector3(55.62, 0, -15.32),
+        new BABYLON.Vector3(49.32, 0, -14.92),
+        new BABYLON.Vector3(45.52, 0, -16.92),
+        new BABYLON.Vector3(44.52, 0, -25.02),
+        new BABYLON.Vector3(42.72, 0, -24.82),
+        new BABYLON.Vector3(42.02, 0, -17.62),
+        new BABYLON.Vector3(39.42, 0, -16.52),
+        new BABYLON.Vector3(38.12, 0, -13.02),
+        new BABYLON.Vector3(41.92, 0, -9.36),
+        new BABYLON.Vector3(50.68, 0, -5.32),
+        new BABYLON.Vector3(51.88, 0, -2.52),
+        new BABYLON.Vector3(48.18, 0, -1.02),
+        new BABYLON.Vector3(43.68, 0, 0.22),
+        new BABYLON.Vector3(37.88, 0, 0.42),
+        new BABYLON.Vector3(37.28, 0, -3.38),
+        new BABYLON.Vector3(33.98, 0, -3.72),
+        new BABYLON.Vector3(33.04, 0, -2.18),
+        new BABYLON.Vector3(38.24, 0, 3.52),
+        new BABYLON.Vector3(46.44, 0, 4.72),
+        new BABYLON.Vector3(46.64, 0, 7.22),
+        new BABYLON.Vector3(39.84, 0, 6.92),
+        new BABYLON.Vector3(36.84, 0, 8.52),
+        new BABYLON.Vector3(36.64, 0, 11.92),
+        new BABYLON.Vector3(40.54, 0, 15.26),
+        new BABYLON.Vector3(40.84, 0, 19.66),
+        new BABYLON.Vector3(34.88, 0, 21.52),
+        new BABYLON.Vector3(32.24, 0, 16.76),
+        new BABYLON.Vector3(30.24, 0, 15.86),
+        new BABYLON.Vector3(29.24, 0, 12.86),
+        new BABYLON.Vector3(27.14, 0, 12.66),
+        new BABYLON.Vector3(26.64, 0, 10.86),
+        new BABYLON.Vector3(30.68, 0, 7.12),
+        new BABYLON.Vector3(32.14, 0, 2.06),
+        new BABYLON.Vector3(26.04, 0, 2.06),
+        new BABYLON.Vector3(23.74, 0, -1.04),
+        new BABYLON.Vector3(26.5, 0, -9.8),
+        new BABYLON.Vector3(29.74, 0, -10.54),
+        new BABYLON.Vector3(31.6, 0, -13.6),
+        new BABYLON.Vector3(29.4, 0, -16.6),
+        new BABYLON.Vector3(24.9, 0, -16.7),
+        new BABYLON.Vector3(23.4, 0, -20.0),
+        new BABYLON.Vector3(25.0, 0, -22.4),
+        new BABYLON.Vector3(26.6, 0, -23.3),
+        new BABYLON.Vector3(25.9, 0, -26.3),
+        new BABYLON.Vector3(32.44, 0, -26.34)
+	]
+
+	const ground = new DestructibleGround(scene, array);
+}
+
+export function createCamera(scene, canvas, pos_x, pos_y, max_distance)
+{
+	const camera = new BABYLON.ArcRotateCamera
+    (
+        "Camera",                               // Name
+        -Math.PI / 2,                           // X-Rotation
+        Math.PI / 2,                            // Y-Rotation
+        max_distance,                          // Distance to Target
+        new BABYLON.Vector3(pos_x, pos_y, 0),   // Target-position
+        scene
+    );
+	camera.attachControl(canvas, true, false);
+	camera.wheelPrecision = 20;
+	camera.minZ = 0.3
+
+	camera.lowerRadiusLimit = 2                 // Minimum Distance of Camera to target
+	camera.upperRadiusLimit = max_distance      // Maximum Distance of Camera to target
+	
+	// No rotating Camera
+	camera.angularSensibilityX = 999999;
+	camera.angularSensibilityY = 999999;
+	// Make right click pan camera
+	camera.useBouncingBehavior = false;
+	camera.fov = 1
+
+	// Limit Camera Movement and zoom
+	scene.beforeRender = function ()
+    {
+		// Formula based on total zoom to determine width when zoomed in
+		const width = camera.upperRadiusLimit * 0.74 -1.475;
+		// Formula to calculate how zoomed in we are
+		const zoom_ratio = (1- ((camera.radius - camera.lowerRadiusLimit) / (camera.upperRadiusLimit - camera.lowerRadiusLimit)));
+		const xLimit = width * zoom_ratio;
+		if (camera.target.x < -xLimit)
+		camera.target.x = -xLimit;
+		else if (camera.target.x > xLimit)
+		camera.target.x = xLimit;
+	}
+
+	return (camera);
+}
+
+
+export async function createScene()
+{
+    // Generate the BABYLON 3D engine
+	const engine = new BABYLON.Engine(canvas, true);
+
+	// Create scene object
+	const scene = new BABYLON.Scene(engine);
+	scene.actionManager = new BABYLON.ActionManager(scene);
+
+	// Add Havoc Phys Engine and Gravity to scene
+	const havokInstance = await HavokPhysics();
+	const hk = new BABYLON.HavokPlugin(true, havokInstance);
+	scene.enablePhysics(new BABYLON.Vector3(0, -9.8, 0), hk);
+
+    var camera = createCamera(scene, canvas, 0, 0, 62)
+
+    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+    light.intensity = 0.7;
+
+    num_players = 4;
+    spawnWorms(scene, num_players);
+	
+	generateGround(scene);
+
+    return (scene);
+};
